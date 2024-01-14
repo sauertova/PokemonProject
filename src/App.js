300621907abb5043f394ba099917c6b12d807d92
Tova Sauer <sauertova1@gmail.com>
2024-01-14 13:48:52 -0500

    added error

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
+
 import './App.css';
+import axios from 'axios';
+import {useState, useEffect} from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [pokemen, setPokemen] = useState([]);
+
+  let getPokemon = async () => {
+    try{
+    const {data} = await axios.get('https://pokeapi.co/api/v2/pokemon')
+    console.log(data);
+    return data.results;
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    (async () => {
+      const pokemenData = await getPokemon();
+      setPokemen((pokemen) => [...pokemenData]);
+    })();
+  }, []);
+
+  return <>
+  <button onClick={() => getPokemon()}>Get Pokemon</button>
+  <div>
+    {pokemen.map((pokemon, index) => {
+      return (
+      <div key ={pokemon.name}>
+        <h3>Name: {pokemon.name}</h3>
+        <p>{pokemon.url}</p>
+        </div>
+    )
+    })}
+  </div>
+  </>;
 }
 
 export default App;
